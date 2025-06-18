1faf16162c5e367855b04125a94eda729da0faab
SebasL <sebas91ts5@gmail.com>
2025-06-18 07:16:11 -0400

    dia fin 1.0

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,8 @@ import { ProtectedRoute } from './ProtectedRoute'
 import { ReservaProvider } from './context/Reserva/ReservaProvider'
 import ClienteRoutes from './routes/ClienteRoutes'
 import { RecetaProvider } from './context/Receta/RecetaProvider'
+import Verificar from './pages/auth/Verificar'
+
 //import { BitacoraPage } from './bitacora/pages/BitacoraPage'
 // import MeseroPedidos from './components/mesero/Pedido'
 
@@ -24,6 +26,7 @@ export default function App() {
               {/* <Route path='/task' element={<Task />} /> */}
               {/* <Route path='/mesero' element={<MeseroPedidos />} /> */}
               <Route element={<ProtectedRoute />}>{DashboardRoutes()}</Route>
+              <Route path='/verificar' element={<Verificar />} />
               {ClienteRoutes()}
             </Routes>
           </RecetaProvider>
